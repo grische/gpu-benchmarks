8dc032ec325024ceb94b4759d520e20120ff0b98
Christian Schmidbauer <ch.schmidbauer@gmail.com>
2018-05-08 11:55:05 +0200

    improve blender setup script

--- blender/setup_benchmarks.sh
+++ blender/setup_benchmarks.sh
@@ -1,15 +1,25 @@
-#! /bin/bash
+#!/bin/bash
+
+DOWNLOAD_FOLDER="$HOME/Downloads"
+TMP_FOLDER="/tmp"
+
+mkdir -p $DOWNLOAD_FOLDER
+cd $TMP_FOLDER/
+
+echo "Downloading and extracting blender files into $TMP_FOLDER"
 
 # Get the blender cycles benchmark files
-wget https://download.blender.org/demo/test/cycles_benchmark_20160228.zip
-unzip cycles_benchmark_20160228.zip
-rm cycles_benchmark_20160228.zip
+wget -c https://download.blender.org/demo/test/cycles_benchmark_20160228.zip -P $DOWNLOAD_FOLDER/  || exit $?
+unzip $DOWNLOAD_FOLDER/cycles_benchmark_20160228.zip || exit $?
+#rm    $DOWNLOAD_FOLDER/cycles_benchmark_20160228.zip || exit $?
+
 
 # Get the blender gooseberry benchmark files and cleanup
-wget http://download.blender.org/demo/test/benchmark.zip
-unzip benchmark.zip  -d ./gooseberry
-rm benchmark.zip
-mv ./gooseberry/benchmark/* ./gooseberry/
-rm -r ./gooseberry/benchmark
-mv gooseberry benchmark/gooseberry
-mv benchmark/gooseberry/benchmark.blend benchmark/gooseberry/gooseberry.blend
+wget -c http://download.blender.org/demo/test/benchmark.zip -P $DOWNLOAD_FOLDER/  || exit $?
+unzip $DOWNLOAD_FOLDER/benchmark.zip -d $TMP_FOLDER/benchmark/gooseberry          || exit $?
+#rm    $DOWNLOAD_FOLDER/benchmark.zip                                              || exit $?
+
+#rearrange gooseberry folder structure
+mv    $TMP_FOLDER/benchmark/gooseberry/benchmark/* $TMP_FOLDER/benchmark/gooseberry/  || exit $?
+rmdir $TMP_FOLDER/benchmark/gooseberry/benchmark                                      || exit $?
+mv    $TMP_FOLDER/benchmark/gooseberry/benchmark.blend benchmark/gooseberry/gooseberry.blend || exit $?
